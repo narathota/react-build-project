fbb9cdcceb35f4354a331d8b4b64b02687a0a467
Kavindu Narathota <kavindun@kasperworld.com>
2016-10-07 12:28:07 +0530

    Routing added

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ListManager = require('./components/ListManager.jsx');
+// var ListManager = require('./components/ListManager.jsx');
+var Routes = require('./Routes.jsx');
 
-ReactDOM.render(<ListManager title="Devices" />, document.getElementById('ingredients'));
+ReactDOM.render(Routes, document.getElementById('ingredients'));
